fb2784fe7782b4e917efe7c2724d7bc313acf359
saimrudu <saimrudula@gmail.com>
2019-09-05 19:09:50 +0000

    tf test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,9 +9,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="http://${placebeard.it}/${500}/${1000}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${saimrudu}'s app. Replace this text with your own. 
   <!-- END -->
   
   </div>
